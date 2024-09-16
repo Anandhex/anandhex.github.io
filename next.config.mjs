f27408f2a20ac79fb30a4779a9b83ac343b68266
Anand Patil <anandpatil@Anands-MacBook-Air.local>
2024-09-16 19:44:31 +0530

    made final fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/anandhex.github.io",
   images: {
     unoptimized: true, // This is necessary as GitHub Pages doesn't support image optimization
   },
